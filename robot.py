7649deb1be70e8081143d1d8e53a525814cb98f7
Mormert <github@mormert.com>
2019-11-13 14:42:51 +0100

    Working robot, can go around the track. Fast. Very fast!

--- robot.py
+++ robot.py
@@ -21,37 +21,56 @@ cl.mode='COL-REFLECT'
 mB = LargeMotor(OUTPUT_A)
 mC = LargeMotor(OUTPUT_B)
 
+timings = 0.5
+
 motors = MoveTank(OUTPUT_A, OUTPUT_B)
 
-while not btn.any():    # avbryt loopen om en knapp trycks.
+def main():
+    try:
+        while not btn.any():    # avbryt loopen om en knapp trycks.
 
-    if cl.value()<25: # om vi är på svart, så kör rakt fram!
-        mB.run_forever(speed_sp=250)
-        mC.run_forever(speed_sp=250)
-    else: # om vi inte är på vitt, så ska vi skanna
+            if cl.value()<25:   # om vi är på svart, så kör rakt fram!
+                mB.run_forever(speed_sp=600)
+                mC.run_forever(speed_sp=600)
+                #motors.run_forever(SpeedPercent(20), SpeedPercent(20))
+                print("Is on black line")
+            else: # om vi inte är på vitt, så ska vi skanna
 
-        blackFound = False
+                blackFound = False
 
-        blackFound = searchForBlackLine(-20,20,1, blackFound)
-        blackFound = searchForBlackLine(20,-20,2, blackFound)
-        blackFound = searchForBlackLine(-20,20,1, blackFound)
+                blackFound = searchForBlackLine(-20,20,timings, blackFound)
+                if blackFound == False:
+                    print("Turning left")
+                blackFound = searchForBlackLine(20,-20,timings*2, blackFound)
+                if blackFound == False:
+                    print("Turning right")
+                blackFound = searchForBlackLine(-20,20,timings, blackFound)
+                if blackFound == False:
+                    print("Going back to ")
 
-        while blackFound == False:
-            blackFound = searchForBlackLine(20,20,1, blackFound)
+                while blackFound == False:
+                    print("Going forward to try and find black line...")
+                    blackFound = searchForBlackLine(40,40,timings*0.5, blackFound)
+                    blackFound = searchForBlackLine(-20,20,timings, blackFound)
+                    blackFound = searchForBlackLine(20,-20,timings*2, blackFound)
+                    blackFound = searchForBlackLine(-20,20,timings, blackFound)
+
+    except Exception as e: print(e)
+        
        
 
 def searchForBlackLine(motor1, motor2, timeSeconds, bf):
     if bf == False:
-        motors.on_for_seconds(SpeedPercent(motor1), SpeedPercent(motor1, timeSeconds, block=False))
-            for i in range(100 * timeSeconds): # skanna 100 ggr
-                if(cl.value()<25): # om vi hittar svart igen
-                    return True
-                sleep(0.01) # skanna var 10 ms
+        motors.on_for_seconds(SpeedPercent(motor1), SpeedPercent(motor2), timeSeconds*2, block=False)
+        for i in range(int(100 * timeSeconds)): # skanna 100 ggr
+            if(cl.value()<25): # om vi hittar svart igen
+                return True
+            sleep(0.01) # skanna var 10 ms
         return False
     else:
         return True
       
-def goAroundObstacle:
+def goAroundObstacle():
 
     turn90DegreeLeft()
     goStraight()
@@ -77,6 +96,13 @@ def turn90DegreeRight():
 
 def goStraight():
     motors.on_for_seconds(SpeedPercent(50), SpeedPercent(50), 5)
+
+main()
+
+#try:
+ #   motors.stop(stop_action='brake')
+  #  motors.run_forever(SpeedPercent(0), SpeedPercent(0))
+#except Exception as e: print(e)
 
 mB.stop(stop_action='brake')
 mC.stop(stop_action='brake')
