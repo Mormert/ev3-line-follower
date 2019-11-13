c06f6bfcf24d22649fc2c84f3c37d35d00b8c3e1
Mormert <github@mormert.com>
2019-11-13 15:55:08 +0100

    Comments added

--- robot.py
+++ robot.py
@@ -6,56 +6,68 @@ from ev3dev2.led import Leds
 from ev3dev2.button import *
 from ev3dev2.sensor.lego import *
 from time   import time, sleep
-#from ev3dev2.motor import SpeedRPM
 
-btn = Button() # will use any button to stop script
+# en knapp för att kunna stänga av programmet
+btn = Button() 
 
-# Connect EV3 color sensor.
+# referens ljussensorn-objektet
 cl = ColorSensor() 
 
-# Put the color sensor into COL-REFLECT mode
-# to measure reflected light intensity.
+# vi kör på "reflect" mode. Under cirka 6-7 i value anser vi som svart linje.
 cl.mode='COL-REFLECT'
 
-# Attach large motors to ports B and C
-mB = LargeMotor(OUTPUT_A)
-mC = LargeMotor(OUTPUT_B)
+# referens till US-sensorn-objektet
+us = UltrasonicSensor()
+# US-sensorn kör i CM (men egentligen mm!)
+us.mode = 'US-DIST-CM'
 
-timings = 0.5
 
+motorA = LargeMotor(OUTPUT_A)   # vi vill ha kontroll över motor A
+motorB = LargeMotor(OUTPUT_B)   # vi vill ha kontroll över motor A
+
+timings = 0.5                   # en konstant variabel som anger hur länge vi ska söka efter svarta linjen
+
+# vi vill också ha enkel kontroll över båda motorerna samtidigt.
 motors = MoveTank(OUTPUT_A, OUTPUT_B)
 
 def main():
     try:
-        while not btn.any():    # avbryt loopen om en knapp trycks.
+        while not btn.any():        # avbryt loopen om en knapp trycks.
+
+            if us.value() < 100:    # om ett objekt är 10 cm från US-sensorn
+                goAroundObstacle()  # gå runt objektet
 
             if cl.value()<25:   # om vi är på svart, så kör rakt fram!
-                mB.run_forever(speed_sp=600)
-                mC.run_forever(speed_sp=600)
-                #motors.run_forever(SpeedPercent(20), SpeedPercent(20))
-                print("Is on black line")
-            else: # om vi inte är på vitt, så ska vi skanna
+                motorA.run_forever(speed_sp=200) # en godtycklig hastighet
+                motorB.run_forever(speed_sp=200) # på båda motorerna
+            else: # om vi inte är på vitt, så ska vi gå in i "scan-mode"
 
                 blackFound = False
 
-                blackFound = searchForBlackLine(-20,20,timings, blackFound)
-                if blackFound == False:
-                    print("Turning left")
-                blackFound = searchForBlackLine(20,-20,timings*2, blackFound)
-                if blackFound == False:
-                    print("Turning right")
-                blackFound = searchForBlackLine(-20,20,timings, blackFound)
-                if blackFound == False:
-                    print("Going back to ")
+                # vi har en bool blackFound, som blir True i funktionen searchForBlackLine
+                # om ljussensorn känner av svart färg. searchForBlackLine-funktionen
+                # körs tills den är klar, för att sedan köras igen, fast med andra argument.
 
+                blackFound = searchForBlackLine(-20,20,timings, blackFound)
+
+                blackFound = searchForBlackLine(20,-20,timings*2, blackFound)
+
+                blackFound = searchForBlackLine(-20,20,timings, blackFound)
+
+
+                # så länge vi inte hittat svart färg så vill vi sakta gå fram
+                # och skanna av miljön, åt vänster, åt höger. Så snart vi hittar
+                # svart igen, så går vi tillbaka till att åka rakt fram på svarta linjen.
                 while blackFound == False:
-                    print("Going forward to try and find black line...")
-                    blackFound = searchForBlackLine(40,40,timings*0.5, blackFound)
+                    blackFound = searchForBlackLine(20,20,timings*0.5, blackFound)
                     blackFound = searchForBlackLine(-20,20,timings, blackFound)
                     blackFound = searchForBlackLine(20,-20,timings*2, blackFound)
                     blackFound = searchForBlackLine(-20,20,timings, blackFound)
 
     except Exception as e: print(e)
+    # det är bra att ha en exception-handler,
+    # för annars fortsätter programmet köra
+    # och man måste reboota EV3-roboten.
         
        
 
@@ -63,48 +75,50 @@ def searchForBlackLine(motor1, motor2, timeSeconds, bf):
     if bf == False:
         motors.on_for_seconds(SpeedPercent(motor1), SpeedPercent(motor2), timeSeconds*2, block=False)
         for i in range(int(100 * timeSeconds)): # skanna 100 ggr
-            if(cl.value()<25): # om vi hittar svart igen
+            if us.value() < 100:                # om ett objekt är 10 cm från US-sensorn
+
+                goAroundObstacle()              # gå runt objektet
+                return False                    # vi måste returnera något för att komma ur denna funktion.
+
+            if(cl.value()<25):                  # om vi hittar svart igen
                 return True
-            sleep(0.01) # skanna var 10 ms
+            sleep(0.01)                         # skanna var 10 ms
         return False
     else:
         return True
       
-def goAroundObstacle():
+def goAroundObstacle():                         # en hårdkodad funtkion för att gå runt ett objekt
 
-    turn90DegreeLeft()
-    goStraight()
-    turn90DegreeRight()
-    goStraight()
+    turn90DegreeLeft(0.3)
+    goStraight(0.7)
+    turn90DegreeRight(0.3)
+    goStraight(0.5)
     turn90DegreeRight()
 
     goUntil = False
-    while goUntil == False:
-        goUntil = searchForBlackLine(10,10,1, goUntil)
+    while goUntil == False:                     # vi kör tills vi hittar tillbaka till den svarta linjen.
+        goUntil = searchForBlackLine(50,50,1, goUntil)
 
-    turn90DegreeLeft()
+    turn90DegreeLeft()                          # när vi hittat svarta linjen igen gör vi en 90-graders-sväng igen.
 
 
 
-def turn90DegreeLeft():
-    #motors.on_for_seconds(SpeedPercent(50), SpeedPercent(-50), 1)
-    motors.on_for_rotations(SpeedPercent(-50), SpeedPercent(50), 3)
+def turn90DegreeLeft(goforsec):
+    motors.on_for_seconds(SpeedPercent(-50), SpeedPercent(50), goforsec)
 
-def turn90DegreeRight():
-    #motors.on_for_seconds(SpeedPercent(-50), SpeedPercent(50), 1)
-    motors.on_for_rotations(SpeedPercent(50), SpeedPercent(-50), 3)
+def turn90DegreeRight(goforsec):
+    motors.on_for_seconds(SpeedPercent(50), SpeedPercent(-50), goforsec)
 
-def goStraight():
-    motors.on_for_seconds(SpeedPercent(50), SpeedPercent(50), 5)
+def goStraight(goforsec):
+    motors.on_for_seconds(SpeedPercent(50), SpeedPercent(50), goforsec)
 
 main()
 
-#try:
- #   motors.stop(stop_action='brake')
-  #  motors.run_forever(SpeedPercent(0), SpeedPercent(0))
-#except Exception as e: print(e)
+#   vi stänger av motorerna i slutet på programmet.
+motorA.stop(stop_action='brake')
+motorB.stop(stop_action='brake')
+motorA.run_forever(speed_sp=0)
+motorB.run_forever(speed_sp=0)
 
-mB.stop(stop_action='brake')
-mC.stop(stop_action='brake')
-mB.run_forever(speed_sp=0)
-mC.run_forever(speed_sp=0)
+#   Johan Lind, 2019 Örebro universitet
+#   GNU GENERAL PUBLIC LICENSE
